0520f4cb19a9cb17bb6c96f731a919745d0df45c
Tom Brown <tom@nworbmot.org>
2023-12-08 18:35:47 +0100

    add rule and script to download ariadne scenario data

---
+++ workflow/scripts/retrieve_ariadne_scenario_data.py
@@ -0,0 +1,19 @@
+
+
+import pyam
+
+pyam.iiasa.set_config(snakemake.config["iiasa_database"]["username"],
+                      snakemake.config["iiasa_database"]["password"])
+
+
+model_raw = pyam.read_iiasa(snakemake.config["iiasa_database"]["db_name"],
+                            model=snakemake.config["iiasa_database"]["model_name"],
+                            scenario=snakemake.config["iiasa_database"]["scenario"])
+
+model_df = model_raw.timeseries()
+
+model_df = model_df.loc[snakemake.config["iiasa_database"]["model_name"],
+                        snakemake.config["iiasa_database"]["scenario"],
+                        snakemake.config["iiasa_database"]["region"]]
+
+model_df.to_csv(snakemake.output.data)
