43916e23075ed02fe8508d7c38ce7895f97d4f01
Tom Brown <tom@nworbmot.org>
2023-12-20 09:44:30 +0100

    change modifications to cost to avoid touching PyPSA-Eur
    Now the output name of retrieve_cost_data is altered instead, then
    this file is modified. This avoids having to touch the input names of
    many rules using cost_{year}.csv.

---
+++ workflow/scripts/modify_cost_data.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+costs = pd.read_csv(snakemake.input.costs, index_col=[0, 1]).sort_index()
+
+modifications = pd.read_csv(snakemake.input.modifications, index_col=[0, 1]).sort_index()
+costs.loc[modifications.index] = modifications
+print(modifications)
+print( costs.loc[modifications.index])
+
+costs.to_csv(snakemake.output[0])
