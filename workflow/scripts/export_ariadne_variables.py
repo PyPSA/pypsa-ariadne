94024db87c1fe05598dfcc73044b669d70070453
Michael Lindner <michaellindner@posteo.de>
2024-02-28 18:04:15 +0100

    first prototype running

--- workflow/scripts/export_ariadne_variables.py
+++ workflow/scripts/export_ariadne_variables.py
@@ -1,4 +1,536 @@
 import pandas as pd
+import pypsa
+from functools import reduce
+from numpy import isclose
+
+# Defining global varibales
+
+TWh2PJ = 3.6
+MWh2TJ = 3.6e-3 
+MW2GW = 1e-3
+t2Mt = 1e-6
+
+MWh2GJ = 3.6
+TWh2PJ = 3.6
+MWh2PJ = 3.6e-6
+
+#n.statistics.withdrawal(bus_carrier="land transport oil", groupby=groupby, aggregate_time=False).filter(like="DE1 0",axis=0)
+
+
+
+def get_capacities_electricity(n, region):
+
+    kwargs = {
+        'groupby': n.statistics.groupers.get_name_bus_and_carrier,
+        'nice_names': False,
+    }
+
+    var = pd.Series()
+
+    capacities_electricity = n.statistics.optimal_capacity(
+        bus_carrier=["AC", "low voltage"],
+        **kwargs,
+    ).filter(like=region).groupby("carrier").sum().drop( 
+        # transmission capacities
+        ["AC", "DC", "electricity distribution grid"],
+    ).multiply(MW2GW)
+
+    var["Capacity|Electricity|Biomass|w/ CCS"] = \
+        capacities_electricity.get('urban central solid biomass CHP CC')
+    
+    var["Capacity|Electricity|Biomass|w/o CCS"] = \
+        capacities_electricity.get('urban central solid biomass CHP')
+
+    var["Capacity|Electricity|Biomass|Solids"] = \
+        var[[
+            "Capacity|Electricity|Biomass|w/ CCS",
+            "Capacity|Electricity|Biomass|w/o CCS",
+        ]].sum()
+
+    # Ariadne does no checks, so we implement our own?
+    assert var["Capacity|Electricity|Biomass|Solids"] == \
+        capacities_electricity.filter(like="solid biomass").sum()
+
+    var["Capacity|Electricity|Biomass"] = \
+        var["Capacity|Electricity|Biomass|Solids"]
+
+
+    var["Capacity|Electricity|Coal|Hard Coal"] = \
+        capacities_electricity.get('coal', 0)                                              
+
+    var["Capacity|Electricity|Coal|Lignite"] = \
+        capacities_electricity.get('lignite', 0)
+    
+    # var["Capacity|Electricity|Coal|Hard Coal|w/ CCS"] = 
+    # var["Capacity|Electricity|Coal|Hard Coal|w/o CCS"] = 
+    # var["Capacity|Electricity|Coal|Lignite|w/ CCS"] = 
+    # var["Capacity|Electricity|Coal|Lignite|w/o CCS"] = 
+    # var["Capacity|Electricity|Coal|w/ CCS"] = 
+    # var["Capacity|Electricity|Coal|w/o CCS"] = 
+    # Q: CCS for coal Implemented, but not activated, should we use it?
+    # !: No, because of Kohleausstieg
+    # > config: coal_cc
+
+
+    var["Capacity|Electricity|Coal"] = \
+        var[[
+            "Capacity|Electricity|Coal|Lignite",
+            "Capacity|Electricity|Coal|Hard Coal",
+        ]].sum()
+
+    # var["Capacity|Electricity|Gas|CC|w/ CCS"] =
+    # var["Capacity|Electricity|Gas|CC|w/o CCS"] =  
+    # ! Not implemented, rarely used   
+
+    var["Capacity|Electricity|Gas|CC"] = \
+        capacities_electricity.get('CCGT')
+    
+    var["Capacity|Electricity|Gas|OC"] = \
+        capacities_electricity.get('OCGT')
+    
+    var["Capacity|Electricity|Gas|w/ CCS"] =  \
+        capacities_electricity.get('urban central gas CHP CC')  
+    
+    var["Capacity|Electricity|Gas|w/o CCS"] =  \
+        capacities_electricity.get('urban central gas CHP') + \
+        var[[
+            "Capacity|Electricity|Gas|CC",
+            "Capacity|Electricity|Gas|OC",
+        ]].sum()
+    
+
+    var["Capacity|Electricity|Gas"] = \
+        var[[
+            "Capacity|Electricity|Gas|w/ CCS",
+            "Capacity|Electricity|Gas|w/o CCS",
+        ]].sum()
+
+    # var["Capacity|Electricity|Geothermal"] = 
+    # ! Not implemented
+
+    var["Capacity|Electricity|Hydro"] = \
+        pd.Series({
+            c: capacities_electricity.get(c) 
+            for c in ["ror", "hydro"]
+        }).sum()
+    # Q!: Not counting PHS here, because it is a true storage,
+    # as opposed to hydro
+     
+    # var["Capacity|Electricity|Hydrogen|CC"] = 
+    # ! Not implemented
+    # var["Capacity|Electricity|Hydrogen|OC"] = 
+    # Q: "H2-turbine"
+    # Q: What about retrofitted gas power plants? -> Lisa
+
+    var["Capacity|Electricity|Hydrogen|FC"] = \
+        capacities_electricity.get("H2 Fuel Cell")
+
+    var["Capacity|Electricity|Hydrogen"] = \
+        var["Capacity|Electricity|Hydrogen|FC"]
+
+    # var["Capacity|Electricity|Non-Renewable Waste"] = 
+    # ! Not implemented
+
+    var["Capacity|Electricity|Nuclear"] = \
+        capacities_electricity.get("nuclear", 0)
+
+    # var["Capacity|Electricity|Ocean"] = 
+    # ! Not implemented
+
+    # var["Capacity|Electricity|Oil|w/ CCS"] = 
+    # var["Capacity|Electricity|Oil|w/o CCS"] = 
+    # ! Not implemented
+
+    var["Capacity|Electricity|Oil"] = \
+        capacities_electricity.get("oil")
+
+
+    var["Capacity|Electricity|Solar|PV|Rooftop"] = \
+        capacities_electricity.get("solar rooftop")
+    
+    var["Capacity|Electricity|Solar|PV|Open Field"] = \
+        capacities_electricity.get("solar") 
+
+    var["Capacity|Electricity|Solar|PV"] = \
+        var[[
+            "Capacity|Electricity|Solar|PV|Open Field",
+            "Capacity|Electricity|Solar|PV|Rooftop",
+        ]].sum()
+    
+    # var["Capacity|Electricity|Solar|CSP"] = 
+    # ! not implemented
+
+    var["Capacity|Electricity|Solar"] = \
+        var["Capacity|Electricity|Solar|PV"]
+    
+    var["Capacity|Electricity|Wind|Offshore"] = \
+        capacities_electricity.get(
+            ["offwind", "offwind-ac", "offwind-dc"]
+        ).sum()
+    # !: take care of "offwind" -> "offwind-ac"/"offwind-dc"
+
+    var["Capacity|Electricity|Wind|Onshore"] = \
+        capacities_electricity.get("onwind")
+    
+    var["Capacity|Electricity|Wind"] = \
+        capacities_electricity.filter(like="wind").sum()
+    
+    assert var["Capacity|Electricity|Wind"] == \
+        var[[
+            "Capacity|Electricity|Wind|Offshore",
+            "Capacity|Electricity|Wind|Onshore",
+        ]].sum()
+
+
+    # var["Capacity|Electricity|Storage Converter|CAES"] = 
+    # ! Not implemented
+
+    var["Capacity|Electricity|Storage Converter|Hydro Dam Reservoir"] = \
+        capacities_electricity.get('hydro')
+    
+    var["Capacity|Electricity|Storage Converter|Pump Hydro"] = \
+        capacities_electricity.get('PHS')
+
+    var["Capacity|Electricity|Storage Converter|Stationary Batteries"] = \
+        capacities_electricity.get("battery discharger") + \
+        capacities_electricity.get("home battery discharger")
+
+    var["Capacity|Electricity|Storage Converter|Vehicles"] = \
+        capacities_electricity.get("V2G", 0)
+    
+    var["Capacity|Electricity|Storage Converter"] = \
+        var[[
+            "Capacity|Electricity|Storage Converter|Hydro Dam Reservoir",
+            "Capacity|Electricity|Storage Converter|Pump Hydro",
+            "Capacity|Electricity|Storage Converter|Stationary Batteries",
+            "Capacity|Electricity|Storage Converter|Vehicles",
+        ]].sum()
+    
+
+    storage_capacities = n.statistics.optimal_capacity(
+        storage=True,
+        **kwargs,
+    ).filter(like=region).groupby("carrier").sum().multiply(MW2GW)
+    # var["Capacity|Electricity|Storage Reservoir|CAES"] =
+    # ! Not implemented
+     
+    var["Capacity|Electricity|Storage Reservoir|Hydro Dam Reservoir"] = \
+        storage_capacities.get("hydro")
+
+    var["Capacity|Electricity|Storage Reservoir|Pump Hydro"] = \
+        storage_capacities.get("PHS")
+    
+    var["Capacity|Electricity|Storage Reservoir|Stationary Batteries"] = \
+        pd.Series({
+            c: storage_capacities.get(c) 
+            for c in ["battery", "home battery"]
+        }).sum()
+    
+    var["Capacity|Electricity|Storage Reservoir|Vehicles"] = \
+        storage_capacities.get("Li ion", 0) 
+
+    var["Capacity|Electricity|Storage Reservoir"] = \
+        var[[
+            "Capacity|Electricity|Storage Reservoir|Hydro Dam Reservoir",
+            "Capacity|Electricity|Storage Reservoir|Pump Hydro",
+            "Capacity|Electricity|Storage Reservoir|Stationary Batteries",
+            "Capacity|Electricity|Storage Reservoir|Vehicles",
+        ]].sum()
+
+
+    var["Capacity|Electricity"] = \
+            var[[
+            "Capacity|Electricity|Wind",
+            "Capacity|Electricity|Solar",
+            "Capacity|Electricity|Oil",
+            "Capacity|Electricity|Coal",
+            "Capacity|Electricity|Gas",
+            "Capacity|Electricity|Biomass",
+            "Capacity|Electricity|Hydro",
+            "Capacity|Electricity|Hydrogen",
+            "Capacity|Electricity|Nuclear",
+            ]].sum()
+
+    # Test if we forgot something
+    _drop_idx = [
+        col for col in [
+            "PHS",
+            "battery discharger",
+            "home battery discharger",
+            "V2G",
+        ] if col in capacities_electricity.index
+    ]
+    assert isclose(
+        var["Capacity|Electricity"],
+        capacities_electricity.drop(_drop_idx).sum(),
+    )
+    
+    return var
+
+def get_capacities_heat(n, region):
+
+    kwargs = {
+        'groupby': n.statistics.groupers.get_name_bus_and_carrier,
+        'nice_names': False,
+    }
+
+    var = pd.Series()
+
+    capacities_heat = n.statistics.optimal_capacity(
+        bus_carrier=[
+            "urban central heat",
+            "urban decentral heat",
+            "rural heat"
+        ],
+        **kwargs,
+    ).filter(like=region).groupby("carrier").sum().drop(
+        ["urban central heat vent"]
+    ).multiply(MW2GW)
+
+
+    var["Capacity|Heat|Solar thermal"] = \
+        capacities_heat.filter(like="solar thermal").sum()
+    # TODO Ariadne DB distinguishes between Heat and Decentral Heat!
+    # We should probably change all capacities here?!
+
+    # !!! Missing in the Ariadne database
+    #  We could be much more detailed for the heat sector (as for electricity)
+    # if desired by Ariadne
+    #
+    var["Capacity|Heat|Biomass|w/ CCS"] = \
+        capacities_heat.get('urban central solid biomass CHP CC') 
+    var["Capacity|Heat|Biomass|w/o CCS"] = \
+        capacities_heat.get('urban central solid biomass CHP') \
+        +  capacities_heat.filter(like="biomass boiler").sum()
+    
+    var["Capacity|Heat|Biomass"] = \
+        var["Capacity|Heat|Biomass|w/ CCS"] + \
+        var["Capacity|Heat|Biomass|w/o CCS"]
+
+    assert isclose(
+        var["Capacity|Heat|Biomass"],
+        capacities_heat.filter(like="biomass").sum()
+    )
+    
+    var["Capacity|Heat|Resistive heater"] = \
+        capacities_heat.filter(like="resistive heater").sum()
+    
+    var["Capacity|Heat|Processes"] = \
+        pd.Series({c: capacities_heat.get(c) for c in [
+                "Fischer-Tropsch",
+                "H2 Electrolysis",
+                "H2 Fuel Cell",
+                "Sabatier",
+                "methanolisation",
+        ]}).sum()
+
+    # !!! Missing in the Ariadne database
+
+    var["Capacity|Heat|Gas"] = \
+        capacities_heat.filter(like="gas boiler").sum() \
+        + capacities_heat.filter(like="gas CHP").sum()
+    
+    # var["Capacity|Heat|Geothermal"] =
+    # ! Not implemented 
+
+    var["Capacity|Heat|Heat pump"] = \
+        capacities_heat.filter(like="heat pump").sum()
+
+    var["Capacity|Heat|Oil"] = \
+        capacities_heat.filter(like="oil boiler").sum()
+
+    var["Capacity|Heat|Storage Converter"] = \
+        capacities_heat.filter(like="water tanks discharger").sum()
+
+    storage_capacities = n.statistics.optimal_capacity(
+        storage=True,
+        **kwargs,
+    ).filter(like=region).groupby("carrier").sum().multiply(MW2GW)
+
+    var["Capacity|Heat|Storage Reservoir"] = \
+        storage_capacities.filter(like="water tanks").sum()
+
+    # Q: New technologies get added as we develop the model.
+    # It would be helpful to have some double-checking, e.g.,
+    # by asserting that every technology gets added,
+    # or by computing the total independtly of the subtotals, 
+    # and summing the subcategories to compare to the total
+    # !: For now, check the totals by summing in two different ways
+    
+    var["Capacity|Heat"] = (
+        var["Capacity|Heat|Solar thermal"] +
+        var["Capacity|Heat|Resistive heater"] +
+        var["Capacity|Heat|Biomass"] +
+        var["Capacity|Heat|Oil"] +
+        var["Capacity|Heat|Gas"] +
+        var["Capacity|Heat|Processes"] +
+        #var["Capacity|Heat|Hydrogen"] +
+        var["Capacity|Heat|Heat pump"]
+    )
+
+    assert isclose(
+        var["Capacity|Heat"],
+        capacities_heat[
+            # exclude storage converters (i.e., dischargers)
+            ~capacities_heat.index.str.contains("discharger")
+        ].sum()
+    )
+
+    return var
+
+
+def get_capacities_other(n, region):
+    kwargs = {
+        'groupby': n.statistics.groupers.get_name_bus_and_carrier,
+        'nice_names': False,
+    }
+
+    var = pd.Series()
+
+    capacities_h2 = n.statistics.optimal_capacity(
+        bus_carrier="H2",
+        **kwargs,
+    ).filter(
+        like=region
+    ).groupby("carrier").sum().multiply(MW2GW)
+
+    var["Capacity|Hydrogen|Gas|w/ CCS"] = \
+        capacities_h2.get("SMR CC")
+    
+    var["Capacity|Hydrogen|Gas|w/o CCS"] = \
+        capacities_h2.get("SMR")
+    
+    var["Capacity|Hydrogen|Gas"] = \
+        capacities_h2.filter(like="SMR").sum()
+    
+    assert var["Capacity|Hydrogen|Gas"] == \
+        var["Capacity|Hydrogen|Gas|w/ CCS"] + \
+        var["Capacity|Hydrogen|Gas|w/o CCS"] 
+    
+    var["Capacity|Hydrogen|Electricity"] = \
+        capacities_h2.get("H2 Electrolysis", 0)
+
+    var["Capacity|Hydrogen"] = (
+        var["Capacity|Hydrogen|Electricity"]
+        + var["Capacity|Hydrogen|Gas"]
+    )
+    assert isclose(
+        var["Capacity|Hydrogen"],
+        capacities_h2.reindex([
+            "H2 Electrolysis",
+            "SMR",
+            "SMR CC",
+        ]).sum(), # if technology not build, reindex returns NaN
+    )
+
+    storage_capacities = n.statistics.optimal_capacity(
+        storage=True,
+        **kwargs,
+    ).filter(like=region).groupby("carrier").sum().multiply(MW2GW)
+
+    var["Capacity|Hydrogen|Reservoir"] = \
+        storage_capacities.get("H2")
+
+
+
+    capacities_gas = n.statistics.optimal_capacity(
+        bus_carrier="gas",
+        **kwargs,
+    ).filter(
+        like=region
+    ).groupby("carrier").sum().drop(
+        # Drop Import (Generator, gas), Storage (Store, gas), 
+        # and Transmission capacities
+        ["gas", "gas pipeline", "gas pipeline new"]
+    ).multiply(MW2GW)
+
+    var["Capacity|Gases|Hydrogen"] = \
+        capacities_gas.get("Sabatier", 0)
+    
+    var["Capacity|Gases|Biomass"] = \
+        capacities_gas.reindex([
+            "biogas to gas",
+            "biogas to gas CC",
+        ]).sum()
+
+    var["Capacity|Gases"] = (
+        var["Capacity|Gases|Hydrogen"] +
+        var["Capacity|Gases|Biomass"] 
+    )
+
+    assert isclose(
+        var["Capacity|Gases"],
+        capacities_gas.sum(),
+    )
+
+
+    capacities_liquids = n.statistics.optimal_capacity(
+        bus_carrier=["oil", "methanol"],
+        **kwargs,
+    ).filter(
+        like=region
+    ).groupby("carrier").sum().multiply(MW2GW)
+
+    var["Capacity|Liquids|Hydrogen"] = \
+        capacities_liquids.get("Fischer-Tropsch") + \
+        capacities_liquids.get("methanolisation", 0)
+    
+    var["Capacity|Liquids"] = var["Capacity|Liquids|Hydrogen"]
+
+    assert isclose(
+        var["Capacity|Liquids"], capacities_liquids.sum(),
+    )
+
+    return var 
+
+def get_ariadne_var(n, industry_demand, energy_totals, region):
+
+    var = pd.concat([
+        get_capacities_electricity(n,region),
+        get_capacities_heat(n,region),
+        get_capacities_other(n,region),
+        #get_primary_energy(n, region),
+        #get_secondary_energy(n, region),
+        #get_final_energy(n, region, industry_demand, energy_totals),
+        #get_prices(n,region), 
+        #get_emissions
+    ])
+
+
+    return var
+
+
+# uses the global variables model, scenario and var2unit. For now.
+def get_data(
+        n, industry_demand, energy_totals, region, 
+        version="0.9.0", scenario="test"
+    ):
+    
+    var = get_ariadne_var(n, industry_demand, energy_totals, region)
+
+    data = []
+    for v in var.index:
+        try:
+            unit = var2unit[v]
+        except KeyError:
+            print("Warning: Variable '", v, "' not in Ariadne Database", sep="")
+            unit = "NA"
+
+        data.append([
+            "PyPSA-Eur " + version, 
+            scenario,
+            region,
+            v,
+            unit,
+            var[v],
+        ])
+
+    tab = pd.DataFrame(
+        data, 
+        columns=["Model", "Scenario", "Region", "Variable", "Unit", year]
+    )
+
+    return tab
 
 if __name__ == "__main__":
     if "snakemake" not in globals():
@@ -15,14 +547,52 @@ if __name__ == "__main__":
             clusters=22,
             opts="",
             ll="v1.2",
-            sector_opts="365H-T-H-B-I-A-solar+p3-linemaxext15",
-            planning_horizons="2040",
+            sector_opts="None",
+            planning_horizons="2050",
+            run="240219-test/normal"
         )
 
 
-print(snakemake.input.networks)
-df=pd.DataFrame()
-df.to_csv(
-    snakemake.output.ariadne_variables,
-    index=False
-)
+    config = snakemake.config
+    var2unit = pd.read_excel(
+        snakemake.input.template, 
+        sheet_name="variable_definitions",
+        index_col="Variable",
+    )["Unit"]
+
+    industry_demands = [
+        pd.read_csv(
+            in_dem, 
+            index_col="TWh/a (MtCO2/a)",
+        ).multiply(TWh2PJ).rename_axis("bus")
+        for in_dem in snakemake.input.industry_demands
+    ]
+    energy_totals = pd.read_csv(
+        snakemake.input.energy_totals,
+        index_col=0,
+    ).multiply(TWh2PJ)
+
+    networks = [pypsa.Network(n) for n in snakemake.input.networks]
+
+    yearly_dfs = []
+    for i, year in enumerate(config["scenario"]["planning_horizons"]):
+        yearly_dfs.append(get_data(
+            networks[i],
+            industry_demands[i],
+            energy_totals,
+            "DE",
+            version=config["version"],
+            scenario=config["run"]["name"][0],
+        ))
+
+    df = reduce(
+        lambda left, right: pd.merge(
+            left, 
+            right, 
+            on=["Model", "Scenario", "Region", "Variable", "Unit"]), 
+        yearly_dfs
+    )
+    df.to_csv(
+        snakemake.output.ariadne_variables,
+        index=False
+    )
