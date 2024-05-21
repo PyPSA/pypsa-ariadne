9da4b825db9920166b4c69476a5b5c4a0ca16ed2
Michael Lindner <michaellindner@posteo.de>
2024-05-21 14:57:30 +0200

    simplify logic

--- workflow/scripts/modify_cost_data.py
+++ workflow/scripts/modify_cost_data.py
@@ -16,48 +16,43 @@ if __name__ == "__main__":
             planning_horizons="2030",
             file_path="../data/costs/",
             file_name="costs_2030.csv",
-            cost_horizon="mean",
+            cost_horizon="pessimist",
+            run="KN2045_Bal_v4"
             )
     logger = logging.getLogger(__name__)
 
     # read in cost data from technology-data library
-    filepath = snakemake.params.file_path + snakemake.params.cost_horizon
-    costs = os.path.join(filepath, snakemake.params.file_name)
+    costs = os.path.join(
+        snakemake.params.file_path, 
+        snakemake.params.cost_horizon, 
+        snakemake.params.file_name)
+
     costs = pd.read_csv(costs, index_col=[0, 1]).sort_index()
 
-    # Pessimist: costs are taken from + 5 years
-    if snakemake.params.cost_horizon == "pessimist":
-        match = re.search(r"costs_(\d{4})-modifications\.csv", snakemake.input[0])
-        if int(match.group(1)) > 2020:
-            new_year = int(match.group(1)) + 5
-            new_filename = re.sub(r'costs_\d{4}-modifications\.csv', f"costs_{new_year}-modifications.csv", snakemake.input[0])
-            modifications = pd.read_csv(new_filename, index_col=[0, 1]).sort_index()
-            costs.loc[modifications.index] = modifications
-            logger.warning(f"Pessimistic cost scenario for {match.group(1)}.")
-        else:
-            modifications = pd.read_csv(snakemake.input.modifications, index_col=[0, 1]).sort_index()
-            costs.loc[modifications.index] = modifications
+    matched_year = int(re.search(
+            r"costs_(\d{4})-modifications\.csv", 
+            snakemake.input.modifications
+        ).group(1))
 
-    # Optimist: costs are taken from - 5 years
+    if matched_year <= 2020 or snakemake.params.cost_horizon == "mean":
+        logger.warning(f"Mean cost scenario for {matched_year}.")
+        new_year = matched_year
+    elif snakemake.params.cost_horizon == "pessimist":
+        logger.warning(f"Pessimistic cost scenario for {matched_year}.")
+        new_year = matched_year + 5
     elif snakemake.params.cost_horizon == "optimist":
-        match = re.search(r"costs_(\d{4})-modifications\.csv", snakemake.input[0])
-        if int(match.group(1)) > 2020:
-            new_year = int(match.group(1)) - 5
-            new_filename = re.sub(r'costs_\d{4}-modifications\.csv', f"costs_{new_year}-modifications.csv", snakemake.input[0])
-            modifications = pd.read_csv(new_filename, index_col=[0, 1]).sort_index()
-            costs.loc[modifications.index] = modifications
-            logger.warning(f"Optimistic cost scenario for {match.group(1)}.")
-        else:
-            modifications = pd.read_csv(snakemake.input.modifications, index_col=[0, 1]).sort_index()
-            costs.loc[modifications.index] = modifications
+        logger.warning(f"Optimistic cost scenario for {matched_year}.")
+        new_year = matched_year - 5
+    else:
+        raise ValueError("Invalid specification of cost options.")
 
-    # Mean: costs are taken from the same year
-    elif snakemake.params.cost_horizon == "mean":
-        modifications = pd.read_csv(snakemake.input.modifications, index_col=[0, 1]).sort_index()
-        costs.loc[modifications.index] = modifications
-        logger.warning("Mean cost scenario.")
+    new_filename = re.sub(
+        r'costs_\d{4}-modifications\.csv', 
+        f"costs_{new_year}-modifications.csv", 
+        snakemake.input.modifications)
+    modifications = pd.read_csv(new_filename, index_col=[0, 1]).sort_index()
+    costs.loc[modifications.index] = modifications
 
-    print(modifications)
-    print( costs.loc[modifications.index])
+    print(costs.loc[modifications.index])
 
     costs.to_csv(snakemake.output[0])
