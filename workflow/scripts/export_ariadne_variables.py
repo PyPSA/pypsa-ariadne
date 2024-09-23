6096165631a48ecd530a66102b26d665f595ba9c
Michael Lindner <michaellindner@posteo.de>
2024-09-23 15:13:07 +0200

    add gross fossil co2 for industry

--- workflow/scripts/export_ariadne_variables.py
+++ workflow/scripts/export_ariadne_variables.py
@@ -2617,12 +2617,18 @@ def get_emissions(n, region, _energy_totals):
     ).sum() + co2_emissions.get("industry methanol", 0)
     # process emissions is mainly cement, methanol is used for chemicals
     # TODO where should the methanol go?
-
-    var["Emissions|CO2|Energy|Demand|Industry"] = co2_emissions.reindex(
-        ["gas for industry", "gas for industry CC", "coal for industry"]
-    ).sum() - co2_atmosphere_withdrawal.get(
-        "solid biomass for industry CC",
-        0,
+    var["Emissions|Gross Fossil CO2|Energy|Demand|Industry"] = (
+        co2_emissions.reindex(
+            [
+                "gas for industry",
+                "gas for industry CC",
+                "coal for industry",
+            ]
+        ).sum()
+    )
+    var["Emissions|CO2|Energy|Demand|Industry"] = (
+        var["Emissions|Gross Fossil CO2|Energy|Demand|Industry"]
+        - co2_atmosphere_withdrawal.get("solid biomass for industry CC", 0)
     )
 
     var["Emissions|CO2|Industry"] = (
@@ -2737,9 +2743,27 @@ def get_emissions(n, region, _energy_totals):
         co2_emissions.get("HVC to air").sum() + waste_CHP_emissions.sum()
     )
 
-    var["Emissions|CO2|Energy|Supply|Liquids and Gases"] = var[
-        "Emissions|CO2|Energy|Supply|Liquids"
-    ] = co2_emissions.get("oil refining", 0)
+    var["Emissions|Gross Fossil CO2|Energy|Supply|Liquids"] = \
+        co2_emissions.get("oil refining", 0)
+
+    var["Emissions|CO2|Energy|Supply|Liquids"] = \
+        var["Emissions|Gross Fossil CO2|Energy|Supply|Liquids"]
+
+    var["Emissions|CO2|Energy|Supply|Liquids and Gases"] = \
+        var["Emissions|CO2|Energy|Supply|Liquids"] # no gases at the moment
+
+    var["Emissions|Gross Fossil CO2|Energy|Supply"] = (
+        var["Emissions|Gross Fossil CO2|Energy|Supply|Electricity"]
+        + var["Emissions|Gross Fossil CO2|Energy|Supply|Heat"]
+        + var["Emissions|Gross Fossil CO2|Energy|Supply|Hydrogen"]
+        + var["Emissions|Gross Fossil CO2|Energy|Supply|Liquids"]
+        # TODO add coke
+    )
+
+    # var["Emissions|Gross Fossil CO2|Energy"] = (
+    #     var["Emissions|Gross Fossil CO2|Energy|Supply"]
+    #     + var["Emissions|Gross Fossil CO2|Energy|Demand|Industry"]
+    # )
 
     var["Emissions|CO2|Energy|Supply"] = (
         var["Emissions|CO2|Energy|Supply|Gases"]
