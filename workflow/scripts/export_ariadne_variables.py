d9a522607d2f676d41781949cd37061222972138
Michael Lindner <michaellindner@posteo.de>
2024-10-02 19:02:50 +0200

    fix missing transmission investments

--- workflow/scripts/export_ariadne_variables.py
+++ workflow/scripts/export_ariadne_variables.py
@@ -333,7 +333,7 @@ def get_investments(n, costs, region):
             "Investment|Energy Supply|Gas|Transmission"
         ]
 
-    return var
+    return pd.concat([var,grid_var])
 
 
 def get_capacity_additions_nstat(n, region):
