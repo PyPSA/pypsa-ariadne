0abf8b69adf160af50171dfe821f2f50e418fce4
Toni Seibold <153275395+toniseibold@users.noreply.github.com>
2024-10-14 17:01:35 +0200

    Restricting powerflow from and to Germany (#231)
    * Restricting powerflow from and to Germany
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Micha <michaellindner@pik-potsdam.de>

--- workflow/scripts/additional_functionality.py
+++ workflow/scripts/additional_functionality.py
@@ -100,6 +100,66 @@ def add_capacity_limits(n, investment_year, limits_capacity, sense="maximum"):
                     sys.exit()
 
 
+def add_power_limits(n, investment_year, limits_power_max):
+    """
+    " Restricts the maximum inflow/outflow of electricity from/to a country.
+    """
+    for ct in limits_power_max:
+        if investment_year not in limits_power_max[ct].keys():
+            continue
+
+        limit = 1e3 * limits_power_max[ct][investment_year] / 10
+
+        logger.info(
+            f"Adding constraint on electricity import/export from/to {ct} to be < {limit} MW"
+        )
+        incoming_line = n.lines.index[
+            (n.lines.carrier == "AC")
+            & (n.lines.bus0.str[:2] != ct)
+            & (n.lines.bus1.str[:2] == ct)
+        ]
+        outgoing_line = n.lines.index[
+            (n.lines.carrier == "AC")
+            & (n.lines.bus0.str[:2] == ct)
+            & (n.lines.bus1.str[:2] != ct)
+        ]
+
+        incoming_link = n.links.index[
+            (n.links.carrier == "DC")
+            & (n.links.bus0.str[:2] != ct)
+            & (n.links.bus1.str[:2] == ct)
+        ]
+        outgoing_link = n.links.index[
+            (n.links.carrier == "DC")
+            & (n.links.bus0.str[:2] == ct)
+            & (n.links.bus1.str[:2] != ct)
+        ]
+
+        # iterate over snapshots - otherwise exporting of postnetwork fails since
+        # the constraints are time dependent
+        for t in n.snapshots:
+            incoming_line_p = n.model["Line-s"].loc[t, incoming_line]
+            outgoing_line_p = n.model["Line-s"].loc[t, outgoing_line]
+            incoming_link_p = n.model["Link-p"].loc[t, incoming_link]
+            outgoing_link_p = n.model["Link-p"].loc[t, outgoing_link]
+
+            lhs = (
+                incoming_link_p.sum()
+                - outgoing_link_p.sum()
+                + incoming_line_p.sum()
+                - outgoing_line_p.sum()
+            ) / 10
+            # divide by 10 to avoid numerical issues
+
+            cname_upper = f"Power-import-limit-{ct}-{t}"
+            cname_lower = f"Power-export-limit-{ct}-{t}"
+
+            n.model.add_constraints(lhs <= limit, name=cname_upper)
+            n.model.add_constraints(lhs >= -limit, name=cname_lower)
+
+            # not adding to network as the shadow prices are not needed
+
+
 def h2_import_limits(n, investment_year, limits_volume_max):
 
     for ct in limits_volume_max["h2_import"]:
@@ -614,6 +674,8 @@ def additional_functionality(n, snapshots, snakemake):
     add_capacity_limits(
         n, investment_year, constraints["limits_capacity_max"], "maximum"
     )
+
+    add_power_limits(n, investment_year, constraints["limits_power_max"])
 
     if int(snakemake.wildcards.clusters) != 1:
         h2_import_limits(n, investment_year, constraints["limits_volume_max"])
